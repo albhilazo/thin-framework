7575344035e8ffe6f9937877d577044f2d916203
Albert Hilazo <albhilazo@gmail.com>
2016-07-31 18:52:18 +0200

    Update ThinController to work with response objects

--- src/ThinFramework/Component/Controller/ThinController.php
+++ src/ThinFramework/Component/Controller/ThinController.php
@@ -2,12 +2,20 @@
 
 namespace ThinFramework\Component\Controller;
 
+use ThinFramework\Component\Response\Response;
+
 
 abstract class ThinController
 {
 
     public function indexAction()
     {
+    }
+
+
+    protected function setResponse($content)
+    {
+        return new Response($content);
     }
 
 }
