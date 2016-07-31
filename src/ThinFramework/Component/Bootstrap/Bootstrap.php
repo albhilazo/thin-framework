4ac7b251a959e39fc14e861544b495493b178459
Albert Hilazo <albhilazo@gmail.com>
2016-07-31 17:05:39 +0200

    Update Bootstrap component with controller action call

--- src/ThinFramework/Component/Bootstrap/Bootstrap.php
+++ src/ThinFramework/Component/Bootstrap/Bootstrap.php
@@ -8,6 +8,8 @@ use ThinFramework\Component\Router\Router;
 class Bootstrap
 {
 
+    const DEFAULT_ACTION = 'indexAction';
+
     private $config;
 
 
@@ -22,6 +24,19 @@ class Bootstrap
         $router = new Router($this->config['routing_path']);
         $currentPath = parse_url($_SERVER['REQUEST_URI'])['path'];
         $route = $router->getRoute($currentPath);
+
+        $routeController = new $route['attributes']['controller']();
+        $routeAction = (isset($route['attributes']['action']))
+                        ? $route['attributes']['action']
+                        : self::DEFAULT_ACTION;
+        $routeParams = (isset($route['parameters']))
+                        ? $route['parameters']
+                        : [];
+
+        call_user_func_array(
+            [$routeController, $routeAction],
+            $routeParams
+        );
     }
 
 }
